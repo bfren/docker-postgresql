bf8bfab24ff339c681ebf4e5840910caa9b5d157
Ben Green <ben@bcgdesign.com>
2022-03-07 12:01:48 +0000

    Adding pull.sh and run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+IMAGE=`cat VERSION`
+POSTGRESQL=${1:-14}
+
+docker buildx build \
+    --build-arg BF_IMAGE=postgresql \
+    --build-arg BF_VERSION=${IMAGE} \
+    -f ${POSTGRESQL}/Dockerfile \
+    -t postgresql${POSTGRESQL}-dev \
+    . \
+    && \
+    docker run -it postgresql${POSTGRESQL}-dev sh
