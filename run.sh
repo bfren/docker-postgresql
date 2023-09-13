6d5064194680bde54e93ec4d0c01de404bc22ed3
bfren <bf@bfren.dev>
2023-09-13 10:41:21 +0100

    Adding --load to run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 IMAGE=`cat VERSION`
-POSTGRESQL=${1:-14}
+POSTGRESQL=${1:-15}
 
 docker buildx build \
+    --load \
     --build-arg BF_IMAGE=postgresql \
     --build-arg BF_VERSION=${IMAGE} \
     -f ${POSTGRESQL}/Dockerfile \
