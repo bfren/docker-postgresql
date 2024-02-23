6756e194a1dc20a60dd20fdb624ab567f946717b
bfren <bf@bfren.dev>
2024-02-23 19:36:41 +0000

    Adding new test framework

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+IMAGE=postgresql
+VERSION=`cat VERSION`
+POSTGRESQL=${1:-16}
+TAG=${IMAGE}-test
+
+docker buildx build \
+    --load \
+    --build-arg BF_IMAGE=${IMAGE} \
+    --build-arg BF_VERSION=${VERSION} \
+    -f ${POSTGRESQL}/Dockerfile \
+    -t ${TAG} \
+    . \
+    && \
+    docker run --entrypoint "/usr/bin/env" ${TAG} -i nu -c "use bf test ; test"
