bbbc18c6cb751aa1e26e8b7b92e078b7641500a1
bfren <bf@bfren.dev>
2025-12-06 09:15:48 +0000

    Adding support for PostgreSQL 18

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -7,7 +7,7 @@ docker pull bfren/alpine
 BASE_VERSION="6.1.0"
 echo "Base: ${BASE_VERSION}"
 
-POSTGRESQL_VERSIONS="12 13 14 15 16 17"
+POSTGRESQL_VERSIONS="12 13 14 15 16 17 18"
 for V in ${POSTGRESQL_VERSIONS} ; do
 
     echo "PostgreSQL ${V}"
